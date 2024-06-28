073140980735d0a9ad56adede735a409b70bd3fd
radicalten <79894181+radicalten@users.noreply.github.com>
2024-06-27 20:16:53 -0500

    Update main.c

--- example/main.c
+++ example/main.c
@@ -6,9 +6,9 @@
 
 #include <SDL2/SDL.h>
 
-#ifdef __wii__
-#include "ogc_keyboard.h"
-#endif
+// #ifdef __wii__
+// #include "ogc_keyboard.h"
+// #endif
 
 static SDL_Window *window;
 static SDL_Renderer *renderer;
